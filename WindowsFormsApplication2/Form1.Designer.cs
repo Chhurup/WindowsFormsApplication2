7130d85ea89fcf58acf3359b2cab59e66e9d20cc
Chhurup <chhurup@gmail.com>
2015-08-14 19:20:22 +0200

    Alle filer submitted
    Commited alle filer fra bærbar

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TrykHer = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // TrykHer
+            // 
+            this.TrykHer.BackColor = System.Drawing.SystemColors.Desktop;
+            this.TrykHer.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.TrykHer.Location = new System.Drawing.Point(64, 96);
+            this.TrykHer.Name = "TrykHer";
+            this.TrykHer.Size = new System.Drawing.Size(148, 98);
+            this.TrykHer.TabIndex = 0;
+            this.TrykHer.Text = "TrykHer";
+            this.TrykHer.UseVisualStyleBackColor = false;
+            this.TrykHer.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.textBox1.Location = new System.Drawing.Point(40, 44);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(468, 349);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TrykHer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button TrykHer;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
